0651112838329bce063ba080faf317d6825464e8
Datacentrics <66388161+datacentrics01@users.noreply.github.com>
2023-11-03 16:38:20 -0300

    Delete modules/autonomous_database directory

--- modules/autonomous_database/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "autonomous_database_ids" {
-  description = "The OCIDs of the Autonomous Databases."
-  value       = { for key, db in oci_database_autonomous_database.adb : key => db.id }
-}
-
-output "autonomous_database_passwords" {
-  description = "The passwords of the Autonomous Databases."
-  value       = { for key, pwd in random_string.autonomous_database_admin_password : key => pwd.result }
-}
