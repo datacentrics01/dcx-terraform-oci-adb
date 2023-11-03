4588766f46da101a965a3ccea977e06f55082740
Datacentrics <66388161+datacentrics01@users.noreply.github.com>
2023-11-03 16:43:52 -0300

    Create main.tf

---
+++ example/main.tf
@@ -0,0 +1,38 @@
+module "autonomous_databases" {
+  source    = "./modules/autonomous_database"
+  instances = {
+    "adb_instance_1" = {
+      compartment_id        = var.compartment_id,
+      cpu_core_count        = 2,
+      data_storage_size_in_tbs = 1,
+      db_name                = "myadb1",
+      db_version             = "19c",
+      display_name           = "MyAutonomousDB1",
+      license_model         = "LICENSE_INCLUDED",
+      db_workload           = "OLTP",
+      is_free_tier          = false,
+      state                  = "STOPPED"
+    },
+    "adb_instance_2" = {
+      compartment_id        = var.compartment_id,
+      cpu_core_count        = 2,
+      data_storage_size_in_tbs = 1,
+      db_name                = "myadb2",
+      db_version             = "19c",
+      display_name           = "MyAutonomousDB2",
+      license_model          = "LICENSE_INCLUDED",
+      db_workload            = "OLTP",
+      is_free_tier           = false,
+      state                  = "STOPPED"
+    }
+    # Add more instances as needed
+  }
+}
+
+output "adb_ids" {
+  value = module.autonomous_databases.autonomous_database_ids
+}
+
+output "adb_passwords" {
+  value = module.autonomous_databases.autonomous_database_passwords
+}
